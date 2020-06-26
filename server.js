372c6d8b05e3532b21e01814097f8af9cfdc0fd8
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-06-26 16:44:18 +0200

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017/bigchungus'
+
+mongoose.connect(url, {useNewUrlParser : true});
+
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening to port 3000');
+});
